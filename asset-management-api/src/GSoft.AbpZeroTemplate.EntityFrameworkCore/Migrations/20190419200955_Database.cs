093c24f99f262ab03e4c5cfaa726c2adf332eef0
ThienLan <meomeocf98@gmail.com>
2019-04-20 03:13:14 +0700

    Update database

---
+++ asset-management-api/src/GSoft.AbpZeroTemplate.EntityFrameworkCore/Migrations/20190419200955_Database.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GSoft.AbpZeroTemplate.Migrations
+{
+    public partial class Database : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dat_Assets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    UpdatedDate = table.Column<DateTime>(nullable: true),
+                    UpdatedBy = table.Column<string>(nullable: true),
+                    IsDelete = table.Column<bool>(nullable: false),
+                    Area = table.Column<string>(nullable: true),
+                    Unitcode = table.Column<string>(nullable: true),
+                    Areacode = table.Column<string>(nullable: true),
+                    Transaction = table.Column<string>(nullable: true),
+                    Assetcode = table.Column<string>(nullable: true),
+                    Assetname = table.Column<string>(nullable: true),
+                    Serinumber = table.Column<string>(nullable: true),
+                    Originalprice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dat_Assets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dat_Assets");
+        }
+    }
+}
